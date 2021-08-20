0d9ce106c5a9c8de5a9479c5a8571450d4cba372
Your Name <nickmihaly@gmail.com>
2021-08-19 21:29:58 -0700

    added get all/id & create routes and controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,53 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    // get all Thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+        .sort({ _id: -1 })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+
+     //get one Thought by id
+     getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+        .then(dbThoughtData => {
+            //If no Thought is found, send 404
+            if(!dbThoughtData) {
+                res.status(404).json({ message: 'No Thought found with this id!' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    },
+    //create Thought
+    addThought({ params, body }, res) {
+        console.log(body);
+        Thought.create(body)
+            .then(({ _id }) => {
+                return User.findOneAndUpdate(
+                    { _id: params.userId },
+                    { $push: { thoughts: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No User found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.json(err));
+},
+}
+
+module.exports = thoughtController;
